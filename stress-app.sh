36529c5763d9450ace27bb1f9bca9ea7ac03b9b0
Jason Mimick <114495365+jasonmimick-aws@users.noreply.github.com>
2025-02-05 14:22:28 -0500

    Update stress-app.sh
    Lookup API_GATEWAY id via AWS cli

--- stress-app.sh
+++ stress-app.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -x
-API_GATEWAY="${1}"
-METHOD="${2:-POST}"
-oha -m ${METHOD} -c 10 -q 10 --latency-correction --disable-keepalive "https://${API_GATEWAY}.execute-api.us-east-1.amazonaws.com/call"
+#API_GATEWAY="${1}"
+API_GATEWAY=$(aws cloudformation describe-stacks --region us-east-1 --stack-name DatadogSimpleApiGateway | \
+jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="apiGatewayId") | .OutputValue')
+HTTP_METHOD="${2:-POST}"
+echo "Running stress test with HTTP_METHOD=${HTTP_METHOD} on APIGATEWAY=${API_GATEWAY}"
+oha -m ${HTTP_METHOD} -c 10 -q 10 --latency-correction --disable-keepalive "https://${API_GATEWAY}.execute-api.us-east-1.amazonaws.com/call"
